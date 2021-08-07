6f0b4ee2fb7fcd04ff67c7593242e4f141e8e098
Cagge <danil.cage@gmail.com>
2021-08-07 21:41:47 +0500

    Add files via upload

--- controllers/control.go
+++ controllers/control.go
@@ -20,59 +20,51 @@ type UpdateAccInput struct {
 func GetAllAcc(context *gin.Context) {
 	var accs []models.Acc
 	models.DB.Find(&accs)
-
 	context.JSON(http.StatusOK, gin.H{"accs": accs})
 }
 
 func GetAcc(context *gin.Context) {
 	var acc models.Acc
 	if err := models.DB.Where("id = ?", context.Param("id")).First(&acc).Error; err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": "Запись не существует"})
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Пользователя с таким ID не существует"})
 		return
 	}
-
 	context.JSON(http.StatusOK, gin.H{"accs": acc})
 }
 
 func CreateAcc(context *gin.Context) {
 	var input CreateAccInput
 	if err := context.ShouldBindJSON(&input); err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Проверьте ключ и повторите ввод"})
 		return
 	}
 
 	acc := models.Acc{Name: input.Name, Position: input.Position}
 	models.DB.Create(&acc)
-
 	context.JSON(http.StatusOK, gin.H{"accs": acc})
 }
 
 func UpdateAcc(context *gin.Context) {
 	var acc models.Acc
 	if err := models.DB.Where("id = ?", context.Param("id")).First(&acc).Error; err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": "Запись не существует"})
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Пользователя с таким ID не существует"})
 		return
 	}
-
 	var input UpdateAccInput
 	if err := context.ShouldBindJSON(&input); err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Пользователя с таким ID не существует"})
 		return
 	}
-
 	models.DB.Model(&acc).Update(input)
-
 	context.JSON(http.StatusOK, gin.H{"acc": acc})
 }
 
 func DeleteAcc(context *gin.Context) {
 	var acc models.Acc
 	if err := models.DB.Where("id = ?", context.Param("id")).First(&acc).Error; err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": "Запись не существует"})
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Пользователя с таким ID не существует"})
 		return
 	}
-
 	models.DB.Delete(&acc)
-
 	context.JSON(http.StatusOK, gin.H{"accs": true})
 }
