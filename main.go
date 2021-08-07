b363107aa4e18da1ef61f078fa62a7fb67c9bd51
Cage <danil.cage@gmail.com>
2021-08-07 21:22:22 +0500

    123

--- main.go
+++ main.go
@@ -8,14 +8,11 @@ import (
 
 func main() {
 	route := gin.Default()
-
 	models.ConnectDB()
-
 	route.GET("/accs", controllers.GetAllAcc)
 	route.POST("/accs", controllers.CreateAcc)
 	route.GET("/accs/:id", controllers.GetAcc)
 	route.PATCH("/accs/:id", controllers.UpdateAcc)
 	route.DELETE("/accs/:id", controllers.DeleteAcc)
-
 	route.Run()
 }
